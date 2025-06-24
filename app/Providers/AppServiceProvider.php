fdd1b14677852715bdaf6f5b2dc168f7147bfabf
Kleyton Santos <edson.kleyton@voxtecnologia.com.br>
2025-06-24 00:47:08 -0300

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,26 @@
 
 namespace App\Providers;
 
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public float $starting_time;
+
+    public float $finished_time;
+
+    /**
+     * The event to listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected array $listen = [];
+
     /**
      * Register any application services.
      */
@@ -19,6 +35,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(CommandStarting::class, function (CommandStarting $event) {
+            $this->starting_time = microtime(true);
+        });
+
+        Event::listen(CommandFinished::class, function (CommandFinished $event) {
+            $this->finished_time = microtime(true);
+            $time = (($this->finished_time - $this->starting_time) / 60); // time in minutes
+            $event->output->writeln("Command [$event->command] takes $time minutes.");
+        });
     }
 }
